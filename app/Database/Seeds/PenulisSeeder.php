0a04f448ac252192e067967199b3cc567e3ca532
amirulmuminin88 <amirmukminin88@gmail.com>
2024-06-19 18:48:49 +0700

    migration and seeding in ci

---
+++ app/Database/Seeds/PenulisSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use CodeIgniter\I18n\Time;
+
+class PenulisSeeder extends Seeder
+{
+    public function run()
+    {
+        /*$data = [
+            [
+                'name' => 'Amirul',
+                'address' => 'Jogoroto',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now()
+            ],
+            [
+                'name' => 'Aldi',
+                'address' => 'Jombang',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now()
+            ],
+            [
+                'name' => 'Ahmad',
+                'address' => 'Jogoroto',
+                'created_at' => Time::now(),
+                'updated_at' => Time::now()
+            ]
+        ];*/
+
+        $faker = \Faker\Factory::create('pt_PT');
+        for ($i = 0; $i < 100; $i++) {
+
+            $data = [
+                'name' => $faker->name,
+                'address' => $faker->address,
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber,
+                'created_at' => Time::createFromTimestamp($faker->unixTime()),
+                'updated_at' => Time::now()
+            ];
+
+            // Simple query
+            //$this->db->query('INSERT INTO penulis (name, address, created_at, updated_at) VALUES(:name:, :address:, :created_at:, :updated_at:)', $data);
+
+            // Using Query Builder
+            $this->db->table('penulis')->insert($data);
+        }
+    }
+}
